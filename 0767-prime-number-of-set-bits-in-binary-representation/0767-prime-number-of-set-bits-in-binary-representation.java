66ad7b9451bc7ce279c9776bbce13cf7482f15b2
Suchethan Reddy Karra <102217992+Suchethan2002@users.noreply.github.com>
2023-10-22 13:26:05 +0530

    Time: 40 ms (28.41%), Space: 38.9 MB (87.62%) - LeetHub

--- 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.java
+++ 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.java
@@ -18,7 +18,15 @@ class Solution {
         {
             return false;
         }
-        for(int i=2;i<=Math.sqrt(n);i++)
+        if(n==2||n==3)
+        {
+            return true;
+        }
+        if(n%2==0)
+        {
+            return false;
+        }
+        for(int i=3;i*i<=n;i+=2)
         {
             if(n%i==0)
             {
